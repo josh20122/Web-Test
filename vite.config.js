7cd3991ea217cb10db1bbc99a5eb6823609bd2f5
josh20122 <jamesjoshua20122@gmail.com>
2022-12-07 07:37:54 +0300

    adding mime type

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'Web-test',
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
